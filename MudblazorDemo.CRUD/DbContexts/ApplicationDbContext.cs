684788c1b1e86175e008e4a78bd9ed8fb4be2f45
Joe <joerosejax@gmail.com>
2024-02-20 11:43:48 -0500

    first commit

---
+++ MudblazorDemo.CRUD/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using MudblazorDemo.CRUD.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MudblazorDemo.CRUD.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
